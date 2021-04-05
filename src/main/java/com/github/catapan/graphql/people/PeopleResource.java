17b01e1a45d77c76b758b849c36527ad57667fcc
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-05 13:28:08 -0300

    feat: add address into getPeople

--- src/main/java/com/github/catapan/graphql/people/PeopleResource.java
+++ src/main/java/com/github/catapan/graphql/people/PeopleResource.java
@@ -1,17 +1,32 @@
 package com.github.catapan.graphql.people;
 
+import java.util.Collections;
 import java.util.List;
+
+import com.github.catapan.graphql.address.Address;
 
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Name;
 import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
 
 @GraphQLApi
 public class PeopleResource {
   
   @Query
-  @Name("getPeoples")
-  public List<People> getPeoples() {
-    return null;
+  @Name("getPeople")
+  public List<People> getPeople() {
+    People people = new People();
+    people.setName("Danilo Catapan");
+    people.setAge(23);
+    return Collections.singletonList(people);
+  }
+
+  @Name("getAddress")
+  public Address getAddress(@Source People people) {
+    Address address = new Address();
+    address.setStreet("Saint Catarina");
+    address.setNumber(123);
+    return address;
   }
 }
