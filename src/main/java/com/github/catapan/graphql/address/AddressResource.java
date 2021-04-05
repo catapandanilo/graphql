8ceecf7f2face2b998e73204fccd4cdc9fc1f3c3
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-04-05 12:09:00 -0300

    feat: add example to getAddress with fake infos

--- src/main/java/com/github/catapan/graphql/address/AddressResource.java
+++ src/main/java/com/github/catapan/graphql/address/AddressResource.java
@@ -1,5 +1,6 @@
 package com.github.catapan.graphql.address;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.microprofile.graphql.GraphQLApi;
@@ -12,6 +13,9 @@ public class AddressResource {
   @Query
   @Name("getAddress")
   public List<Address> getAddress() {
-    return null;
+    Address address = new Address();
+    address.setStreet("Saint Catarina");
+    address.setNumber(123);
+    return Collections.singletonList(address);
   }
 }
